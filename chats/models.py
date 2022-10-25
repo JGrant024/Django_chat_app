63ec05e96726db53de8b2b28c348be0afb419cfb
JGrant024 <jonathan.grant23@icloud.com>
2022-10-25 14:16:32 -0400

    updated models

--- chats/models.py
+++ chats/models.py
@@ -4,19 +4,21 @@ from django.conf import settings
 # Create your models here.
 
 
-class Room(models.Model):
-    title = models.CharField(max_length=255)
-    author = models.CharField(max_length=255)
-    image = models.ImageField(upload_to="Room", null=True)
+class Chat(models.Model):
+    room = models.CharField(max_length=255)
+    text = models.CharField(max_length=255)
+    image = models.ImageField(upload_to="Chat", null=True)
 
     def __str__(self):
         return self.title
 
-# update text book and author 
+# update text book and author
+
+
 class Message(models.Model):
     text = models.TextField()
-    book = models.ForeignKey(Room, on_delete=models.CASCADE)
-    author = models.ForeignKey(
+    user = models.ForeignKey(Chat, on_delete=models.CASCADE)
+    admin = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE, blank=True)
 
     def __str__(self):
